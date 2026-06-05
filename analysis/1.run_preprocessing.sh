313a65d6c0665fc037989b763a851f02371b1c72
DaeHee Hwang <daeheehwang@DaeHees-MacBook-Pro.local>
2026-06-04 23:30:29 -0400

    Set up GPX4 analysis for Terra

--- analysis/1.run_preprocessing.sh
+++ analysis/1.run_preprocessing.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 # Run only the preprocess rules
-snakemake --use-conda --cores all \
+#
+snakemake --use-conda --cores all\
     --snakefile "../brieflow/workflow/Snakefile" \
     --configfile "config/config.yml" \
     --rerun-triggers mtime \
-    --until all_preprocess -n
+    --rerun-incomplete \
+    --resources ic_exclusive=100 \
+    --default-resources ic_exclusive=1 \
+    --set-resources calculate_ic_sbs:ic_exclusive=50 calculate_ic_phenotype:ic_exclusive=100 \
+    --until all_preprocess
